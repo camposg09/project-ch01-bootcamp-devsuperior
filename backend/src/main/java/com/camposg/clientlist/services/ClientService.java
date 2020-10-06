db92adaa86c89757e25da7777f82e2ad465a1aa0
Gustavo Campos <g09.campos@gmail.com>
2020-10-06 13:43:57 -0300

    Update, delete

--- backend/src/main/java/com/camposg/clientlist/services/ClientService.java
+++ backend/src/main/java/com/camposg/clientlist/services/ClientService.java
@@ -2,7 +2,10 @@ package com.camposg.clientlist.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -15,15 +18,15 @@ import com.camposg.clientlist.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository repository;
-	
+
 	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
-		Page<Client> list = repository.findAll(pageRequest); 
+		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(x -> new ClientDTO(x));
-		
+
 	}
 
 	@Transactional(readOnly = true)
@@ -41,6 +44,27 @@ public class ClientService {
 		return new ClientDTO(entity);
 	}
 
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e){
+			throw new ResourceNotFoundException("Id not found: " + id);			
+		}
+	}
+	
+	@Transactional
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		} 
+	}
+
 	private void copyDtoToEntity(ClientDTO dto, Client entity) {
 		entity.setName(dto.getName());
 		entity.setCpf(dto.getCpf());
@@ -49,5 +73,5 @@ public class ClientService {
 		entity.setChildren(dto.getChildren());
 	}
 
-	
+
 }
