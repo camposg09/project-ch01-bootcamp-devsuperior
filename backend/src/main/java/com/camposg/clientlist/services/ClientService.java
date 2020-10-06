b52fe0b1c0340f55711953543c1b436f513ce85d
Gustavo Campos <g09.campos@gmail.com>
2020-10-06 10:23:03 -0300

    Clients by id, exception handling

--- backend/src/main/java/com/camposg/clientlist/services/ClientService.java
+++ backend/src/main/java/com/camposg/clientlist/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.camposg.clientlist.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.camposg.clientlist.dto.ClientDTO;
 import com.camposg.clientlist.entities.Client;
 import com.camposg.clientlist.repositories.ClientRepository;
+import com.camposg.clientlist.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -21,6 +24,13 @@ public class ClientService {
 		Page<Client> list = repository.findAll(pageRequest); 
 		return list.map(x -> new ClientDTO(x));
 		
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
 	}
 
 	
